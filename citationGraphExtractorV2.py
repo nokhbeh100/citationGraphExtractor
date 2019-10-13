62263dc75b0f9649eea98bd7cd5c27c6e551115f
nokhbeh100 <nokhbeh100@gmail.com>
2019-10-12 22:06:08 -0400

    bug of double arrows fixed

--- citationGraphExtractorV2.py
+++ citationGraphExtractorV2.py
@@ -40,66 +40,74 @@ def expand(paperid):
 
     time.sleep(T)
     
-    pages = driver.find_elements_by_css_selector('div.au-target.page')
-    if len(pages) == 0:
-        titles = driver.find_elements_by_css_selector('a.title.au-target')
-        for t in titles:
-            newid = strip_for_id(t.get_property('href'))
-            g.add_node(newid, label=t.text)
-            g.add_edge(paperid, newid)
-
-    else:
-        noOfPages = min([int(pages[-1].text), pagesLimit])
-        for pageNo in range(1, noOfPages+1):
-            print ('page:',pageNo,'/',noOfPages)
-            pages = driver.find_elements_by_css_selector('div.au-target.page')
-            pages[[p.text for p in pages].index(str(pageNo))].click()
-            time.sleep(T)
+    if( 'reference' in driver.current_url ):        
+        pages = driver.find_elements_by_css_selector('div.au-target.page')
+        if len(pages) == 0:
             titles = driver.find_elements_by_css_selector('a.title.au-target')
             for t in titles:
                 newid = strip_for_id(t.get_property('href'))
                 g.add_node(newid, label=t.text)
                 g.add_edge(paperid, newid)
+    
+        else:
+            noOfPages = min([int(pages[-1].text), pagesLimit])
+            for pageNo in range(1, noOfPages+1):
+                print ('page:',pageNo,'/',noOfPages)
+                pages = driver.find_elements_by_css_selector('div.au-target.page')
+                pages[[p.text for p in pages].index(str(pageNo))].click()
+                time.sleep(T)
+                titles = driver.find_elements_by_css_selector('a.title.au-target')
+                for t in titles:
+                    newid = strip_for_id(t.get_property('href'))
+                    g.add_node(newid, label=t.text)
+                    g.add_edge(paperid, newid)
                 
     print ('citations:')
     url = 'https://academic.microsoft.com/paper/'+paperid+'/citedby'
     driver.get(url)
 
     time.sleep(T)
-    
-    pages = driver.find_elements_by_css_selector('div.au-target.page')
-    if len(pages) == 0:
-        titles = driver.find_elements_by_css_selector('a.title.au-target')
-        for t in titles:
-            newid = strip_for_id(t.get_property('href'))
-            g.add_node(newid, label=t.text)
-            g.add_edge(newid, paperid)
-    else:                
-        noOfPages = min([int(pages[-1].text), pagesLimit])
-        for pageNo in range(1, noOfPages+1):
-            print ('page:',pageNo,'/',noOfPages)
-            pages = driver.find_elements_by_css_selector('div.au-target.page')
-            pages[[p.text for p in pages].index(str(pageNo))].click()
-            time.sleep(T)
+    if( 'citedby' in driver.current_url ):            
+        pages = driver.find_elements_by_css_selector('div.au-target.page')
+        if len(pages) == 0:
             titles = driver.find_elements_by_css_selector('a.title.au-target')
             for t in titles:
                 newid = strip_for_id(t.get_property('href'))
                 g.add_node(newid, label=t.text)
                 g.add_edge(newid, paperid)
+        else:                
+            noOfPages = min([int(pages[-1].text), pagesLimit])
+            for pageNo in range(1, noOfPages+1):
+                print ('page:',pageNo,'/',noOfPages)
+                pages = driver.find_elements_by_css_selector('div.au-target.page')
+                pages[[p.text for p in pages].index(str(pageNo))].click()
+                time.sleep(T)
+                titles = driver.find_elements_by_css_selector('a.title.au-target')
+                for t in titles:
+                    newid = strip_for_id(t.get_property('href'))
+                    g.add_node(newid, label=t.text)
+                    g.add_edge(newid, paperid)
                 
     networkx.write_gexf(g, 'outputGraph.gexf')
 
 #%%
 expand('2796885425')     
-expand('2902883805')
+expand('2895739182')
 for tek in range(10):
     for n in deepcopy(g.node):
         if not(n in seen):
             if g.degree[n] > 1:
                 expand(n)
+g.in_degree                
 #tabs = driver.find_elements_by_css_selector('ma-call-to-action.au-target.route')
 #REF = tabs[[t.text for t in tabs].index('REFERENCES')]
 #CIT = tabs[[t.text for t in tabs].index('CITED BY')]
 # create our graph that will get populated
 
 #driver.close()
+                
+#%%
+for n in deepcopy(g.node):
+    if g.degree[n] == 1:
+        g.remove_node(n)
+networkx.write_gexf(g, 'output_clean.gexf')
